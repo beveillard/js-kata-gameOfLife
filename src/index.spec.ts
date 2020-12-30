c074db80e10806e37e06cc4e18b4c565d7a6067c
Benoît Eveillard <benoit.eveillard@gmail.com>
2020-12-29 21:07:34 -0500

    feature(GameOfLife) implement

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,95 @@
-import { systemUnderTest } from ".";
+import { gameOfLife } from ".";
 
-describe("Test of systemUnderTest()", function() {
-  it("Should pass", function() {
-    expect(systemUnderTest()).toEqual(true);
+describe("Test of gameOfLife()", function () {
+
+  it("Should die due to underpopulation", function () {
+    expect(gameOfLife(
+      [
+        [0, 0, 0, 0],
+        [0, 1, 1, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+      ]
+    )).toEqual(
+      [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+      ]
+    );
   });
+
+  it("Should survive", function () {
+    expect(gameOfLife(
+      [
+        [0, 0, 0, 0],
+        [0, 1, 1, 0],
+        [0, 1, 1, 0],
+        [0, 0, 0, 0],
+      ]
+    )).toEqual(
+      [
+        [0, 0, 0, 0],
+        [0, 1, 1, 0],
+        [0, 1, 1, 0],
+        [0, 0, 0, 0],
+      ]
+    );
+  });
+
+  it("Example step #1", function () {
+    expect(gameOfLife(
+      [
+        [1, 1, 1, 0],
+        [1, 1, 1, 0],
+        [1, 1, 1, 0],
+        [0, 0, 0, 0],
+      ]
+    )).toEqual(
+      [
+        [1, 0, 1, 0],
+        [0, 0, 0, 1],
+        [1, 0, 1, 0],
+        [0, 1, 0, 0],
+      ]
+    );
+  });
+
+  it("Example step #2", function () {
+    expect(gameOfLife(
+      [
+        [1, 0, 1, 0],
+        [0, 0, 0, 1],
+        [1, 0, 1, 0],
+        [0, 1, 0, 0],
+      ]
+    )).toEqual(
+      [
+        [0, 0, 0, 0],
+        [0, 0, 1, 1],
+        [0, 1, 1, 0],
+        [0, 1, 0, 0],
+      ]
+    );
+  });
+
+  it("Example step #3", function () {
+    expect(gameOfLife(
+      [
+        [0, 0, 0, 0],
+        [0, 0, 1, 1],
+        [0, 1, 1, 0],
+        [0, 1, 0, 0],
+      ]
+    )).toEqual(
+      [
+        [0, 0, 0, 0],
+        [0, 1, 1, 1],
+        [0, 1, 0, 1],
+        [0, 1, 1, 0],
+      ]
+    );
+  });
+
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,63 @@
-export function systemUnderTest(): boolean {
-  return true;
+export function gameOfLife(board) {
+  let newGenerationBoard = [];
+
+  board.forEach((row, rowIndex) => {
+    pushNewGenerationRow(newGenerationBoard, [board, rowIndex, row]);
+  });
+
+  return newGenerationBoard;
+}
+
+export function pushNewGenerationRow(newGenerationBoard, [board, rowIndex, row]) {
+  let newGenerationRow = [];
+
+  row.forEach((cell, cellIndex) => {
+    pushNewGenerationCell(newGenerationRow, [board, rowIndex, cellIndex, cell]);
+  });
+
+  newGenerationBoard.push(newGenerationRow);
+}
+
+export function pushNewGenerationCell(newGenerationRow, [board, rowIndex, cellIndex, cell]) {
+  let nbNeighbors = countNeighbors(board, rowIndex, cellIndex);
+
+  if (cell > 0) {
+    if (nbNeighbors < 2) newGenerationRow.push(0);
+    else if (nbNeighbors < 4) newGenerationRow.push(1);
+    else newGenerationRow.push(0);
+  } else if (nbNeighbors == 3) {
+    newGenerationRow.push(1);
+  } else {
+    newGenerationRow.push(0);
+  }
+}
+
+export function countNeighbors(board, rowIndex, cellIndex) {
+  let count = 0;
+
+  if (rowIndex > 0) count += countNeighborsInOtherRow(board, rowIndex - 1, cellIndex);
+  count += countNeighborsInSameRow(board, rowIndex, cellIndex);
+  if (rowIndex < board.length - 1) count += countNeighborsInOtherRow(board, rowIndex + 1, cellIndex);
+
+  return count;
+}
+
+export function countNeighborsInOtherRow(board, rowIndex, cellIndex) {
+  let count = 0;
+  let row = board[rowIndex];
+
+  if (cellIndex > 0) count += row[cellIndex - 1] > 0 ? 1 : 0;
+  count += row[cellIndex] > 0 ? 1 : 0;
+  if (cellIndex < row.length - 1) count += row[cellIndex + 1] > 0 ? 1 : 0;
+
+  return count;
+}
+export function countNeighborsInSameRow(board, rowIndex, cellIndex) {
+  let count = 0;
+  let row = board[rowIndex];
+
+  if (cellIndex > 0) count += row[cellIndex - 1] > 0 ? 1 : 0;
+  if (cellIndex < row.length - 1) count += row[cellIndex + 1] > 0 ? 1 : 0;
+
+  return count;
 }
